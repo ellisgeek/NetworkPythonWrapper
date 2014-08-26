2513a97610d7d892d27b456e46059ee0fc7be208
ellisgeek <ellisgeek@live.com>
2014-08-26 15:42:39 -0500

    Made comments more visible
    Batch file comments suck...

--- runpythonscript.bat
+++ runpythonscript.bat
@@ -1,7 +1,7 @@
 @echo off
 cls
 
-rem Edit these variables to fit your environment
+rem  # Edit these variables to fit your environment
 SET     PP_Share=\\<server>\<share>\<folder>
 SET     PP_Drive=Y:
 SET      PP_Path=%PP_Drive%\Python27
@@ -12,7 +12,7 @@ SET Script_Drive=Z:
 
 
 
-rem If no file is specified print some usage info
+rem  # If no file is specified print some usage info
 IF "%1"=="" (
   echo "Usage:"
   echo "runpythonscript <script name>"
@@ -21,31 +21,31 @@ IF "%1"=="" (
 )
 
 
-rem Share containing a copy of portable python
+rem  # Share containing a copy of portable python
 NET USE %PP_Drive% "%PP_Share%" > nul
 
-rem Share containing your python scripts
+rem  # Share containing your python scripts
 NET USE %Script_Drive% "%Script_Share%" > nul
 
-rem Backup the current computers path
+rem  # Backup the current computers path
 SET PATH_TMP=%PATH%
 
-rem Add portable python folder to the path
+rem  # Add portable python folder to the path
 SET PATH="%PATH%;%PP_Path%"
 
-rem Drop from current folder to folder containing portable python
+rem  # Drop from current folder to folder containing portable python
 PUSHD "%PP_Path%"
 
-rem Execute script
+rem  # Execute script
 "%PP_exe%" "%Script_Drive%\%1"
 
-rem Jump back to previous directory
+rem  # Jump back to previous directory
 POPD
 
-rem Set path back to what it was
+rem  # Set path back to what it was
 SET PATH=%PATH_TMP%
 
-rem Unmount Shares
+rem  # Unmount Shares
 NET USE %PP_Drive% /DELETE /Y > nul
 NET USE %Script_Drive% /DELETE /Y > nul
 
